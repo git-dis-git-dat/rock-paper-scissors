a4b68383379533d7ed76c3c96893b7985ec90e91
git-dis-git-dat <too5juan@protonmail.com>
2024-07-08 11:36:00 -0400

    setup

---
+++ app.js
@@ -0,0 +1,34 @@
+"use strict"
+
+function getComputerChoice(){
+    // return r, p, or s using math.random and console log it
+}
+
+function getHumanChoice(){
+    // get r, p, or s from user via prompt and return it, and console log it
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    /*
+        round logic
+
+        humanChoice should be case insensitive
+
+        console.log round winner
+
+        increment humanScore or computerScore based on round winner
+    */
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+function playGame(){
+    //move playRound() in here and score variables
+    //5 found game
+}
