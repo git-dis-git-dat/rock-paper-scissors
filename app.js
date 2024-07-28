318593e9efa559c56df2a01c79f5871a69474b8c
git-dis-git-dat <too5juan@protonmail.com>
2024-07-28 12:33:20 -0400

    fixed erroneous closure

--- app.js
+++ app.js
@@ -29,32 +29,34 @@ function playGame(){
 
     function playRound(humanChoice, computerChoice) {
     // round logic
-    if (humanChoice === "rock" && computerChoice === "scissors"){
-        console.log("nice! " + `${humanChoice}` + " beats " + `${computerChoice}`)
-        return humanScore++;
-    } else if (humanChoice === "paper" && computerChoice === "rock"){
-        console.log("nice! " + `${humanChoice}` + " beats " + `${computerChoice}`)
-        return humanScore++;
-    } else if (humanChoice === "scissors" && computerChoice === "paper"){
-        console.log("nice! " + `${humanChoice}` + " beats " + `${computerChoice}`)
-        return humanScore++;
-    } else if (humanChoice === computerChoice){
-        console.log("Tie")
-        return
-    } else {
-        console.log("try again! " + `${computerChoice}` + " beats " + `${humanChoice}`)
-        return computerScore++;
+        if (humanChoice === "rock" && computerChoice === "scissors"){
+            console.log("nice! " + `${humanChoice}` + " beats " + `${computerChoice}`)
+            return humanScore++;
+        } else if (humanChoice === "paper" && computerChoice === "rock"){
+            console.log("nice! " + `${humanChoice}` + " beats " + `${computerChoice}`)
+            return humanScore++;
+        } else if (humanChoice === "scissors" && computerChoice === "paper"){
+            console.log("nice! " + `${humanChoice}` + " beats " + `${computerChoice}`)
+            return humanScore++;
+        } else if (humanChoice === computerChoice){
+            console.log("Tie")
+            return
+        } else {
+            console.log("try again! " + `${computerChoice}` + " beats " + `${humanChoice}`)
+            return computerScore++;
+        }
+    }
+
+    let roundCount = 0;
+
+    while (roundCount < 5) {
+        roundCount++
+        playRound(getHumanChoice(), getComputerChoice());
+        // if (humanScore === 3 || computerScore === 3) {return alert("Game Over")}
     }
 }
 
-let roundCount = 0;
 
-while (roundCount < 5) {
-    roundCount++
-    playRound(getHumanChoice(), getComputerChoice());
-    // if (humanScore === 3 || computerScore === 3) {return alert("Game Over")}
-}
-}
 
 playGame();
 
